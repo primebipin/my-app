66c4fc8fd1b5a48aaaae8acac9a57f1041da891f
Bipin kumar <brgaya9431@gmail.com>
2023-12-07 21:43:21 +0530

    Updates

---
+++ precache-manifest.cc8fc300b3873433436b96babcd21611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa474eb5fda0ad2d0d5e74ace1ec50e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cfc725d4c7cea382e6e3",
+    "url": "/my-app/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/my-app/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/my-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "cfc725d4c7cea382e6e3",
+    "url": "/my-app/static/js/main.47946bf8.chunk.js"
+  },
+  {
+    "revision": "330a69b442416f742e8c",
+    "url": "/my-app/static/js/runtime~main.148f857f.js"
+  }
+]);
